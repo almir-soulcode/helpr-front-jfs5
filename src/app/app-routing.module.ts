42762b35e010cb9918b281bcf0eaa3acdf38606d
f-gabriel-braga <braagaa.inf@hotmail.com>
2022-12-01 17:23:38 -0300

    Criação componente Home e Cabeçalho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import("./views/login/login.module").then(m => m.LoginModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
   }
 ];
 
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   exports: [
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
