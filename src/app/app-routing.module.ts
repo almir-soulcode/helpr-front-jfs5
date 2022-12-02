03d8038dc1e8bc99b117c3b708db1604f535f177
f-gabriel-braga <braagaa.inf@hotmail.com>
2022-12-02 09:09:31 -0300

    Criação do AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+    canActivate: [ AuthGuard ]
   }
 ];
 
