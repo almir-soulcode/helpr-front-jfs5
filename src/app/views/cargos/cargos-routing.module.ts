a74e57fe9e98cb771d3aa26a978209cd14dedf09
aeromandy <aeromandy2016@gmail.com>
2022-12-10 14:43:38 -0300

    pafina novo cargo

--- src/app/views/cargos/cargos-routing.module.ts
+++ src/app/views/cargos/cargos-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CargosComponent } from './cargos/cargos.component';
+import { NewCargoComponent } from './new-cargo/new-cargo.component';
 
-const routes: Routes = [{ path: '', component: CargosComponent }];
+const routes: Routes = [
+  { 
+    path: '', component: CargosComponent 
+  },
+  {
+    path: 'new',
+    component: NewCargoComponent
+  },
+  {
+    path: 'edit/:id',
+    component: CargosComponent
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/cargos/cargos.module.ts
+++ src/app/views/cargos/cargos.module.ts
@@ -4,20 +4,25 @@ import { CommonModule } from '@angular/common';
 import { CargosRoutingModule } from './cargos-routing.module';
 import { CargosComponent } from './cargos/cargos.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentsModule } from 'src/app/components/components.module';
+import { NewCargoComponent } from './new-cargo/new-cargo.component';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
   declarations: [
-    CargosComponent
+    CargosComponent,
+    NewCargoComponent
   ],
   imports: [
     CommonModule,
     CargosRoutingModule,
     ComponentsModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    MatTableModule
   ]
 })
 export class CargosModule { }
